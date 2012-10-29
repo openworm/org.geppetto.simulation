c14923a2580c4738eacb313bcb85c135dd744565
Giovanni Idili <giovanni.idili@gmail.com>
2012-10-29 16:55:48 +0000

    Working example of dynamic service discovery with custom filter

--- src/main/java/org/openworm/simulationengine/simulation/TestServlet.java
+++ src/main/java/org/openworm/simulationengine/simulation/TestServlet.java
@@ -12,6 +12,7 @@ import org.openworm.simulationengine.core.model.IModelInterpreter;
 import org.openworm.simulationengine.core.simulator.ISimulator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.InvalidSyntaxException;
 import org.osgi.framework.ServiceReference;
 
 /**
@@ -26,13 +27,14 @@ public class TestServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		try {
 
-			IModelInterpreter modelInterpreter = this.<IModelInterpreter>getService("sphModelInterpreter", IModelInterpreter.class.getName());
+			String discoveryID = "sphModelInterpreter";
+			IModelInterpreter modelInterpreter = this.<IModelInterpreter>getService(discoveryID, IModelInterpreter.class.getName());
 			
 			if(modelInterpreter == null){
 				response.getWriter().println("modelInterpreter is null");
 			}
 			else{
-				response.getWriter().println("modelInterpreter: " + modelInterpreter.toString());
+				response.getWriter().println("modelInterpreter found: " + modelInterpreter.toString());
 			}
 		} catch (Exception e) {
 			response.getWriter().println("error: " + e.getMessage());
@@ -42,12 +44,15 @@ public class TestServlet extends HttpServlet {
 	/*
 	 * A generic routine to encapsulate boiler-plate code for dynamic service discovery
 	 */
-	private <T> T getService(String discoveryId, String type){
+	private <T> T getService(String discoveryId, String type) throws InvalidSyntaxException{
 		T service = null;
 		
-		//String filter = String.format("(discoverable-id=%s)", discoveryId);
-		ServiceReference sr  =  _bc.getServiceReference(type);
-		service = (T) _bc.getService(sr);
+		String filter = String.format("(discoverableID=%s)", discoveryId);
+		ServiceReference[] sr  =  _bc.getServiceReferences(type, filter);
+		if(sr != null && sr.length > 0)
+		{
+			service = (T) _bc.getService(sr[0]);
+		}
 		
 		return service;
 	}

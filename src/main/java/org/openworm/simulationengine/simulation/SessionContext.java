e668aba3c9a24652f845dbc9f652090135f6c855
Giovanni Idili <giovanni.idili@gmail.com>
2012-10-31 14:37:32 +0000

    cleanup and formatting

--- src/main/java/org/openworm/simulationengine/simulation/SessionContext.java
+++ src/main/java/org/openworm/simulationengine/simulation/SessionContext.java
@@ -9,25 +9,25 @@ import org.openworm.simulationengine.core.model.IModel;
 import org.openworm.simulationengine.core.model.IModelInterpreter;
 import org.openworm.simulationengine.core.simulator.ISimulator;
 
-public class SessionContext {
-
+public class SessionContext
+{
 	public ConcurrentHashMap<String, HashMap<String, List<IModel>>> modelsByAspect = new ConcurrentHashMap<String, HashMap<String, List<IModel>>>();
-	
+
 	public ConcurrentHashMap<String, IModelInterpreter> modelInterpretersByAspect = new ConcurrentHashMap<String, IModelInterpreter>();
-	
+
 	public ConcurrentHashMap<String, ISimulator> simulatorsByAspect = new ConcurrentHashMap<String, ISimulator>();
-	
+
 	public ConcurrentHashMap<String, String> modelURLByAspect = new ConcurrentHashMap<String, String>();
-	
+
 	public ConcurrentHashMap<String, Integer> processedElementsByAspect = new ConcurrentHashMap<String, Integer>();
-	
+
 	public ConcurrentHashMap<String, Integer> elementCountByAspect = new ConcurrentHashMap<String, Integer>();
-	
+
 	public List<String> aspectIDs = new ArrayList<String>();
-	
+
 	public int processedAspects = 0;
 
-	public boolean runningCycle=false;
+	public boolean runningCycle = false;
 
-	public boolean runSimulation=false;
+	public boolean runSimulation = false;
 }
--- src/main/java/org/openworm/simulationengine/simulation/SimulationCallbackListener.java
+++ src/main/java/org/openworm/simulationengine/simulation/SimulationCallbackListener.java
@@ -6,25 +6,27 @@ import java.util.List;
 import org.openworm.simulationengine.core.model.IModel;
 import org.openworm.simulationengine.core.simulation.ISimulationCallbackListener;
 
-public class SimulationCallbackListener implements ISimulationCallbackListener {
+public class SimulationCallbackListener implements ISimulationCallbackListener
+{
 
 	private String simulationAspectID;
 	private SessionContext sessionContext;
-	
-	public SimulationCallbackListener(String aspectID, SessionContext context) {
+
+	public SimulationCallbackListener(String aspectID, SessionContext context)
+	{
 		this.simulationAspectID = aspectID;
 		this.sessionContext = context;
 	}
-	
+
 	/**
 	 * Callback to populate results buffers.
 	 * 
-	 * @see
-	 * org.openworm.simulationengine.core.simulation.ISimulationCallbackListener
-	 * #resultReady(java.util.List)
+	 * @see org.openworm.simulationengine.core.simulation.ISimulationCallbackListener
+	 *      #resultReady(java.util.List)
 	 */
 	@Override
-	public void resultReady(final List<IModel> models) {
+	public void resultReady(final List<IModel> models)
+	{
 		// when the callback is received results are appended
 		appendResults(models);
 	}
@@ -34,33 +36,24 @@ public class SimulationCallbackListener implements ISimulationCallbackListener {
 	 * 
 	 * @param models
 	 */
-	private void appendResults(List<IModel> models) {
-		
-		if(!sessionContext.modelsByAspect.containsKey(simulationAspectID))
+	private void appendResults(List<IModel> models)
+	{
+
+		if (!sessionContext.modelsByAspect.containsKey(simulationAspectID))
 		{
 			sessionContext.modelsByAspect.put(simulationAspectID, new HashMap<String, List<IModel>>());
 		}
 
 		String modelId = models.get(0).getId();
-		if (sessionContext.modelsByAspect.get(simulationAspectID).containsKey(modelId)) {
-			
-			// NOTE: not sure how the segment below applies to the generic simulation
-			// NOTE: it was originally designed for dealing with plots (multiple timesteps)
-			// check if we have more steps than can be displayed
-			/*if (getSessionContext()._models.get(receivedId).size() >= (config.getViewport() / config.getDt()) / config.getSamplingPeriod()) {
-				// if we have more steps that can be displayed - remove the difference
-				for (int i = 0; i < models.size(); i++) {
-					// always remove the first - when removing everything gets
-					// shifted
-					getSessionContext()._models.get(receivedId).remove(0);
-				}
-			}*/
-			
+		if (sessionContext.modelsByAspect.get(simulationAspectID).containsKey(modelId))
+		{
 			sessionContext.modelsByAspect.get(simulationAspectID).get(modelId).addAll(models);
-		} else {
+		}
+		else
+		{
 			sessionContext.modelsByAspect.get(simulationAspectID).put(modelId, models);
 		}
-		
+
 		Integer processed = sessionContext.processedElementsByAspect.get(simulationAspectID);
 		sessionContext.processedElementsByAspect.replace(simulationAspectID, processed + 1);
 	}

5148b3bd184bd121d4111855520fdd7017efc478
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2016-04-08 11:26:40 +0200

    Data source tests

---
+++ src/test/java/org/geppetto/simulation/test/TestQueryProcessor.java
@@ -0,0 +1,112 @@
+package org.geppetto.simulation.test;
+/*******************************************************************************
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2011 - 2015 OpenWorm.
+ * http://openworm.org
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the MIT License
+ * which accompanies this distribution, and is available at
+ * http://opensource.org/licenses/MIT
+ *
+ * Contributors:
+ *     	OpenWorm - http://openworm.org/people.html
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights 
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+ * copies of the Software, and to permit persons to whom the Software is 
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE 
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *******************************************************************************/
+
+
+import org.geppetto.core.datasources.GeppettoDataSourceException;
+import org.geppetto.core.datasources.IQueryProcessor;
+import org.geppetto.core.features.IFeature;
+import org.geppetto.core.model.GeppettoModelAccess;
+import org.geppetto.core.services.GeppettoFeature;
+import org.geppetto.core.services.registry.ServicesRegistry;
+import org.geppetto.model.DataSource;
+import org.geppetto.model.ProcessQuery;
+import org.geppetto.model.QueryResults;
+import org.geppetto.model.types.CompositeType;
+import org.geppetto.model.types.ImportType;
+import org.geppetto.model.types.TypesFactory;
+import org.geppetto.model.variables.Variable;
+import org.geppetto.model.variables.VariablesFactory;
+
+/**
+ * @author matteocantarelli
+ *
+ */
+public class TestQueryProcessor implements IQueryProcessor
+{
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.geppetto.core.datasources.IQueryProcessor#process(org.geppetto.model.ProcessQuery, org.geppetto.model.variables.Variable, org.geppetto.model.QueryResults)
+	 */
+	@Override
+	public QueryResults process(ProcessQuery query, DataSource dataSource, Variable variable, QueryResults results, GeppettoModelAccess geppettoModelAccess) throws GeppettoDataSourceException
+	{
+		CompositeType type = TypesFactory.eINSTANCE.createCompositeType();
+		type.setId(variable.getId());
+		variable.getAnonymousTypes().add(type);
+		
+		Variable importTypeVar = VariablesFactory.eINSTANCE.createVariable();
+		importTypeVar.setId("testImportVar");
+		importTypeVar.setName("testImportVar");
+		
+		ImportType importType=TypesFactory.eINSTANCE.createImportType();
+		importType.setId("testImportType");
+		importType.setName("testImportType");
+		importType.setModelInterpreterId("testModelInterpreter");
+		importType.setUrl("http://geppetto.org");
+		
+		geppettoModelAccess.addTypeToLibrary(importType, dataSource.getDependenciesLibrary().get(0));
+		
+		importTypeVar.getTypes().add(importType);
+		type.getVariables().add(importTypeVar);
+
+		return results;
+	}
+
+	@Override
+	public void registerGeppettoService() throws Exception
+	{
+		ServicesRegistry.registerQueryProcessorService(this);
+	}
+
+	@Override
+	public boolean isSupported(GeppettoFeature feature)
+	{
+		return false;
+	}
+
+	@Override
+	public IFeature getFeature(GeppettoFeature feature)
+	{
+		return null;
+	}
+
+	@Override
+	public void addFeature(IFeature feature)
+	{
+
+	}
+
+}

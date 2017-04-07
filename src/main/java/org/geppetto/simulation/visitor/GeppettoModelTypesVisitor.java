cf09f9568bf950e2ee1c29384fc2f045c6ab77b7
jrmartin <yeshuarmartinez@gmail.com>
2017-04-06 23:06:20 -0700

    uncomments remaining tests and adds uncommitted class

---
+++ src/main/java/org/geppetto/simulation/visitor/GeppettoModelTypesVisitor.java
@@ -0,0 +1,121 @@
+package org.geppetto.simulation.visitor;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+import org.geppetto.core.manager.Scope;
+import org.geppetto.core.s3.S3Manager;
+import org.geppetto.core.utilities.URLReader;
+import org.geppetto.core.utilities.Zipper;
+import org.geppetto.model.GeppettoPackage;
+import org.geppetto.model.types.ImportType;
+import org.geppetto.model.types.util.TypesSwitch;
+import org.geppetto.model.util.GeppettoModelTraversal;
+import org.geppetto.model.util.GeppettoVisitingException;
+import org.geppetto.simulation.manager.RuntimeProject;
+
+public class GeppettoModelTypesVisitor extends TypesSwitch<Object>
+{
+	private Map<String, String> replaceMap = new HashMap<String, String>();
+	private Path localGeppettoModelFile;
+	private Scope scope;
+	private RuntimeProject runtimeProject;
+	private Zipper zipper;
+	private String URLIdentifier = "amazon.s3";
+
+	@Override
+	public Object caseImportType(ImportType type)
+	{
+		try
+		{
+
+			if(type.eContainingFeature().getFeatureID() == GeppettoPackage.GEPPETTO_LIBRARY__TYPES)
+			{
+				String fullPath = type.getUrl();
+				String newPath = this.getRelativePath(fullPath);
+
+				//only replace URLs for local and amazon instance paths, not other hhttp
+				if(fullPath.contains(URLIdentifier)||!fullPath.startsWith("http")){
+					URL url = URLReader.getURL(fullPath);
+
+					replaceMap.put(fullPath, newPath);
+
+					// let's replace every occurrence of the original URLs inside the file with their copy
+					replaceURLs(localGeppettoModelFile, replaceMap);				
+
+					//user visitor to traverse through Geppetto Model
+					Path localGeppettoTypeFile = Paths.get(URLReader.createLocalCopy(scope, this.runtimeProject.getGeppettoProject().getId(), url, false).toURI());
+					GeppettoModelVisitor dependentModelsVisitor = new GeppettoModelVisitor(localGeppettoTypeFile,this.runtimeProject, zipper);
+					GeppettoModelTraversal.apply(this.runtimeProject.getGeppettoModel(), dependentModelsVisitor);
+					
+					//add geppetto model that was modified to zip
+					zipper.addToZip(localGeppettoTypeFile.toUri().toURL());
+				}
+			}
+		}
+		catch(Exception e)
+		{
+			return new GeppettoVisitingException(e);
+		}
+		return super.caseImportType(type);
+	}
+
+	/**
+	 * Makes a fullpath become a relativve path
+	 * @param fullPath
+	 * @return
+	 */
+	public String getRelativePath(String fullPath){
+		String[] fullPathSplit = fullPath.split("/");
+		String relativePath = "/"+fullPathSplit[fullPathSplit.length-1];
+		
+		return relativePath;
+	}
+	
+	/**
+	 * This method replaces in the localFile all the occurrences of the old URLs with the new ones
+	 * 
+	 * @param localFile
+	 * @param replaceMap
+	 * @throws IOException
+	 */
+	private void replaceURLs(Path localFile, Map<String, String> replaceMap) throws IOException
+	{
+		Charset charset = StandardCharsets.UTF_8;
+
+		String content = new String(Files.readAllBytes(localFile), charset);
+		for(String old : replaceMap.keySet())
+		{
+			content = content.replaceAll(Pattern.quote(old), replaceMap.get(old));
+		}
+		Files.write(localFile, content.getBytes(charset));
+	}
+
+	public void processLocalGeppettoFile() throws IOException
+	{
+		replaceURLs(localGeppettoModelFile, replaceMap);
+	}
+	
+	/**
+	 * @param projectId
+	 * @param scope
+	 * @param libraryManager
+	 */
+	public GeppettoModelTypesVisitor(Path localGeppettoModelFile,RuntimeProject runtimeProject,Zipper zipper, Scope scope)
+	{
+		super();
+		this.runtimeProject = runtimeProject;
+		this.zipper = zipper;
+		this.localGeppettoModelFile = localGeppettoModelFile;
+		this.scope=scope;
+	}
+
+}
